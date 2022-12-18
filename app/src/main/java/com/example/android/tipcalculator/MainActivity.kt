652c9d5e3d712b700e4007924c6245bb64f4dac6
KonradMaslowiec <konrad.maslowiec@intive.com>
2022-12-18 20:01:16 +0100

    tip_calculator code lab completed

--- app/src/main/java/com/example/android/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/android/tipcalculator/MainActivity.kt
@@ -21,12 +21,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.android.tipcalculator.ui.theme.TipCalculatorTheme
+import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +46,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipTimeScreen() {
-
+    var input by remember { mutableStateOf("") }
+    val amount = input.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
 
     Column(
         modifier = Modifier.padding(32.dp),
@@ -52,29 +56,32 @@ fun TipTimeScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = stringResource(id = R.string.calculate_tip),
+            text = stringResource(id = R.string.calculate_tip, tip),
             fontSize = 24.sp,
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
         Spacer(modifier = Modifier.height(16.dp))
-        EditNumberField()
+        EditNumberField(value = input, onValueChange = {input = it})
         Spacer(modifier = Modifier.height(16.dp))
-        Text(text = stringResource(R.string.tip_amount))
+        Text(text = stringResource(R.string.tip_amount, tip))
     }
 }
 
 
 @Composable
-fun EditNumberField() {
-    var input by remember { mutableStateOf("") }
+fun EditNumberField(value : String, onValueChange : (String) -> Unit) {
     TextField(
-        value = input,
-        onValueChange = { input = it },
+        value = value,
+        onValueChange = onValueChange,
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         singleLine = true,
-        label = { Text(text = stringResource(R.string.cost_of_service)) }
-
+        label = { Text(text = stringResource(R.string.cost_of_service), color = Color.Cyan) }
     )
+}
+
+fun calculateTip(amount : Double, tipPercent : Double = 15.0) : String{
+    val tip = tipPercent / 100 * amount
+    return NumberFormat.getCurrencyInstance().format(tip).toString()
 }
 
 @Preview(showBackground = true)
